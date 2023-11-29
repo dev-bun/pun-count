c2ec5fdec3b3198b147f802b805dc07e42f2692e
bchurton <ben@churton.uk>
2023-11-29 13:11:23 +0000

    h

---
+++ pages/index.js
@@ -0,0 +1,68 @@
+import { useState, useEffect } from 'react'
+
+export default function Home() {
+  const [puns, setPuns] = useState(0)
+
+  const [punClass, setPunClass] = useState(`pun0`)
+  const [punSubtext, setPunSubtext] = useState(``)
+
+  const getUniqueSubtext = () => {
+    const allPunSubtexts = [
+      `Phew, no puns (yet)`,
+      `Oh god, it's starting...`,
+      `Send help`,
+      `Getting worse...`,
+      `Tell my family I love them`,
+      `I can't take it anymore`,
+      `You've ruined it`,
+      `It's gone too far`,
+      `I'm done`,
+      `HELLPPPPPPPPP`,
+      `AGHHHHHHH THE PAIN`,
+      `I'm in agony`,
+      `Please make it stop`,
+      `Is there an off switch?`,
+      `When will it end?`,
+      `The pain never ends`,
+      `I'm in hell`
+    ]
+
+    const uniqueSubtexts = allPunSubtexts.filter(subtext => subtext !== punSubtext)
+
+    return uniqueSubtexts[Math.floor(Math.random() * uniqueSubtexts.length)]
+  }
+
+  useEffect(() => {
+    if (puns <= 5) {
+      setPunClass(`pun${puns}`)
+    } else {
+      setPunClass(`punTooMany`)
+    }
+
+    switch (puns) {
+      case 0:
+        setPunSubtext(`Phew, no puns (yet)`)
+        break
+      case 1:
+        setPunSubtext(`Oh god, it's starting...`)
+        break
+      default:
+        setPunSubtext(getUniqueSubtext())       
+        break
+    }
+
+  }, [puns])
+
+  return (
+    <div className={`flex flex-col items-center justify-center min-h-screen py-2 select-none cursor-pointer ${punClass}`} onClick={() => setPuns(puns + 1)}>
+      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
+        <h1 className="text-9xl font-bold">
+          {puns}
+        </h1>
+        <h2 className="text-4xl font-bold">
+          {punSubtext}
+        </h2>
+      </main>
+    </div>
+  )
+}
