0fc52b954a21288b3c82c3e080dc683c0b0c4c2a
bchurton <ben@churton.uk>
2023-11-30 15:05:47 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,21 @@ export default function Home() {
       `Is there an off switch?`,
       `When will it end?`,
       `The pain never ends`,
-      `I'm in hell`
+      `I'm in hell`,
+      `Oh lord. Oh god. Oh no.`,
+      `cool cool cool no doubt no doubt no doubt`,
+      `Shit.`,
+      `I want to go home now.`,
+      `I'm going to cry.`,
+      `Going clinically insane.`,
+      `Thanks Cameron, nan's crying now.`,
+      `Brought a tear to my eyes.`,
+      `Live, laugh, love. Just kidding.`,
+      `Summoning the devil...`,
+      `Please hold, your call is important to us.`,
+      `AGHHHHHHHH`,
+      `The pain is unbearable`,
+      `What happened to the 5 pun limit?`,
     ]
 
     const uniqueSubtexts = allPunSubtexts.filter(subtext => subtext !== punSubtext)
@@ -72,7 +86,7 @@ export default function Home() {
   }, [])
 
   useEffect(() => {
-    if (puns % 5 === 0 && puns !== 0) reward();
+    if (puns % 1000 === 0 && puns !== 0) reward();
 
     if (puns <= 5) {
       setPunClass(`pun${puns}`)
@@ -103,13 +117,26 @@ export default function Home() {
 
   }, [puns])
 
-  const handleClick = () => setPuns(puns + 1);
+  const handleIncrease = () => {
+    // IF ITS PUN #5, SHOW A POPUP SAYING ARE YOU SURE YOU WANT TO CONTINUE
+    if (puns === 5) {
+      if (window.confirm(`We have hit the 5 pun limit. Are you sure you want to proceed?`)) {
+        setPuns(puns + 1)
+      }
+    }
+    else {
+      setPuns(puns + 1)
+    }
+  };
 
   // on spacebar press
   useEffect(() => {
     const handleKeyDown = (e) => {
-      if (e.code === 'Space') {
-        setPuns(puns + 1)
+      if ([13, 32, 38].includes(e.keyCode)) {
+        handleIncrease()
+      }
+      else if (e.keyCode === 40) {
+        setPuns(puns - 1)
       }
     }
 
@@ -121,7 +148,7 @@ export default function Home() {
   }, [puns])
 
   return (
-    <div className={`flex flex-col items-center justify-center min-h-screen py-2 select-none cursor-pointer ${punClass}`} onClick={handleClick}>
+    <div className={`overflow-hidden flex flex-col items-center justify-center min-h-screen py-2 select-none cursor-pointer ${punClass}`} onClick={handleIncrease}>
       <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
         <h1 className="text-9xl font-bold" id="rewardId">
           {puns}
